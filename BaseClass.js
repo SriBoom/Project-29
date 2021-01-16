b9959669f5745eb62b5d1facc598884a9e42887d
SriBoom <74517999+SriBoom@users.noreply.github.com>
2021-01-16 09:59:50 -0800

    Add files via upload

--- BaseClass.js
+++ BaseClass.js
@@ -1,7 +1,7 @@
 class BaseClass{
   constructor(x, y, width, height) {
       var options = {
-         isStatic:false
+         isStatic: false
       }
       this.body = Bodies.rectangle(x, y, width, height, options);
       this.width = width;
